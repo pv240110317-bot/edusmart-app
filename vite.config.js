2c1a9f93053975f1809c82fb7ad9ac520e944bdb
Agualoka <agualoka@example.com>
2025-11-19 14:16:33 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/edusmart-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
